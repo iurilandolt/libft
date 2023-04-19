896689d7aced15adccbee27986d83a4dcc98ec67
rlandolt <rlandolt@c2r10s6.42lisboa.com>
2023-04-19 15:35:49 +0100

    fixes

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: rlandolt <rlandolt@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 03:02:53 by rlandolt          #+#    #+#             */
-/*   Updated: 2023/04/18 18:57:05 by rlandolt         ###   ########.fr       */
+/*   Updated: 2023/04/19 15:07:35 by rlandolt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ static char	*substr_cpy(const char *src, size_t len)
 char	**ft_split(char const *str, char c)
 {
 	char	**split;
-	int		str_i;
+	int		sub_i;
 	int		i;
 	int		j;
 
@@ -60,7 +60,7 @@ char	**ft_split(char const *str, char c)
 	split = (char **)malloc((substr_count(str, c) + 1) * sizeof(char *));
 	if (!split)
 		return (NULL);
-	str_i = 0;
+	sub_i = 0;
 	i = 0;
 	while (*(str + i))
 	{
@@ -70,8 +70,8 @@ char	**ft_split(char const *str, char c)
 		while (*(str + i) && *(str + i) != c)
 			i++;
 		if (j < i)
-			*(split + str_i++) = substr_cpy(&str[j], (i - j) + 1);
+			*(split + sub_i++) = substr_cpy(&str[j], (i - j) + 1);
 	}
-	*(split + str_i) = NULL;
+	*(split + sub_i) = NULL;
 	return (split);
 }
