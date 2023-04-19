4071cb57fd0bf3f22e089b6a9369b725d77dfb50
rlandolt <rlandolt@c2r10s6.42lisboa.com>
2023-04-19 14:10:57 +0100

    fixed mem leak

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: rlandolt <rlandolt@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 15:43:38 by rlandolt          #+#    #+#             */
-/*   Updated: 2023/04/18 13:51:28 by rlandolt         ###   ########.fr       */
+/*   Updated: 2023/04/19 14:10:39 by rlandolt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_substr(char const *str, unsigned int start, size_t len)
 		return (NULL);
 	if (ft_strlen(str) <= start)
 		return (ft_strdup(""));
-	if (len > (ft_strlen(str) + 1) - start)
+	if (len > ft_strlen(str) - start)
 	{
 		len = ft_strlen(str) - start;
 		subs = (char *)malloc(sizeof(char) * (len + 1));
@@ -38,22 +38,20 @@ char	*ft_substr(char const *str, unsigned int start, size_t len)
 	*(subs + i) = '\0';
 	return (subs);
 }
+
 /*
 char	*ft_substr(char const *str, unsigned int start, size_t len)
 {
 	char			*subs;
-	unsigned int	strsize;
 	size_t			i;
 
-	i = 0;
-	strsize = ft_strlen(str);
 	if (!str)
 		return (NULL);
-	if (strsize <= start)
+	if (ft_strlen(str) <= start)
 		return (ft_strdup(""));
-	if (len > (strsize + 1) - start)
+	if (len > ((ft_strlen(str) + 1) - start))
 	{
-		len = strsize - start;
+		len = (ft_strlen(str)) - start;
 		subs = (char *)malloc(sizeof(char) * (len + 1));
 		if (!subs)
 			return (NULL);
@@ -62,12 +60,9 @@ char	*ft_substr(char const *str, unsigned int start, size_t len)
 		subs = (char *)malloc(sizeof(char) * (len + 1));
 	if (!subs)
 		return (NULL);
-	while (i < len)
-	{
-		*(subs + i) = *(str + start);
-		start++;
-		i++;
-	}
+	i = -1;
+	while (++i < len)
+		*(subs + i) = *(str + start + i);
 	*(subs + i) = '\0';
 	return (subs);
 }
