f8ca98ad8913919ff9434304b970821b66104c79
rlandolt <rlandolt@c2r10s6.42lisboa.com>
2023-04-19 15:45:28 +0100

    .subfolders

---
+++ .comments/ft_substr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlandolt <rlandolt@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/15 15:43:38 by rlandolt          #+#    #+#             */
+/*   Updated: 2023/04/19 13:56:29 by rlandolt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *str, unsigned int start, size_t len)
+{
+	char			*subs;
+	size_t			i;
+
+	if (!str)
+		return (NULL);
+	if (ft_strlen(str) <= start)
+		return (ft_strdup(""));
+		/*return empty string with null terminator*/
+	if (len > ((ft_strlen(str) + 1) - start))
+	{
+		/*if len is > str - start, reduce len*/
+		len = (ft_strlen(str)) - start;
+		subs = (char *)malloc(sizeof(char) * (len + 1));
+		if (!subs)
+			return (NULL);
+	}
+	else
+		subs = (char *)malloc(sizeof(char) * (len + 1));
+	if (!subs)
+		return (NULL);
+	i = -1;
+	while (++i < len)
+		*(subs + i) = *(str + start + i);
+	*(subs + i) = '\0';
+	return (subs);
+}
