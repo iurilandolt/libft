b1706b21be518d56e5da080af8254faf59658e14
rlandolt <rlandolt@c1r9s1.42lisboa.com>
2023-05-02 14:23:35 +0100

    fixed mem leak - function now has more than 25 lines...

--- str/ft_split.c
+++ str/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: rlandolt <rlandolt@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 03:02:53 by rlandolt          #+#    #+#             */
-/*   Updated: 2023/04/27 13:37:02 by rlandolt         ###   ########.fr       */
+/*   Updated: 2023/05/02 14:15:45 by rlandolt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,14 @@ static char	*substr_cpy(const char *src, size_t len)
 	return (dest);
 }
 
+static char	**ft_clean(char **array, int i)
+{
+	while (--i >= 0)
+		free(*(array + i));
+	free (array);
+	return (NULL);
+}
+
 char	**ft_split(char const *str, char c)
 {
 	char	**split;
@@ -70,7 +78,11 @@ char	**ft_split(char const *str, char c)
 		while (*(str + i) && *(str + i) != c)
 			i++;
 		if (j < i)
+		{
 			*(split + sub_i++) = substr_cpy(&str[j], (i - j) + 1);
+			if (!*(split + sub_i - 1))
+				return (ft_clean(split, sub_i));
+		}
 	}
 	*(split + sub_i) = NULL;
 	return (split);
